c68c6375900ae44ffebbbcd7bb74eb5beb35e715
Danny <dannyhyunsoowilliams@gmail.com>
2022-04-10 03:23:45 +0100

    move to ondevice folder

--- App.tsx
+++ App.tsx
@@ -1,2 +1,2 @@
-import StorybookUIRoot from './.storybook/Storybook';
-export { StorybookUIRoot as default };
+import StorybookUIRoot from './.ondevice/Storybook';
+export {StorybookUIRoot as default};
