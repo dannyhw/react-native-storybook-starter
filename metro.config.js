37d639f44bbae7359243f19f191486b3fcc95f58
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-09-15 10:28:13 +0100

    feat: update packages

--- metro.config.js
+++ metro.config.js
@@ -1,42 +1,18 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const path = require('path');
-
-const {generate} = require('@storybook/react-native/scripts/generate');
-
-generate({
-  configPath: path.resolve(__dirname, './.ondevice'),
-});
-
+const withStorybook = require('@storybook/react-native/metro/withStorybook');
+const defaultConfig = getDefaultConfig(__dirname);
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
-  transformer: {
-    unstable_allowRequireContext: true,
-  },
-  resolver: {
-    resolveRequest: (context, moduleName, platform) => {
-      const defaultResolveResult = context.resolveRequest(
-        context,
-        moduleName,
-        platform,
-      );
+const config = {};
 
-      if (
-        process.env.STORYBOOK_ENABLED !== 'true' &&
-        defaultResolveResult?.filePath?.includes?.('.ondevice/')
-      ) {
-        return {
-          type: 'empty',
-        };
-      }
+const finalConfig = mergeConfig(defaultConfig, config);
 
-      return defaultResolveResult;
-    },
-  },
-};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withStorybook(finalConfig, {
+    enabled: process.env.STORYBOOK_ENABLED === 'true',
+    configPath: path.resolve(__dirname, './.ondevice'),
+});
